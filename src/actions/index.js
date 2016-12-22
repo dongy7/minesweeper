5435a522c3be77bfe5d58200485b0cf9772ccb80
dongy7 <dongy7@gmail.com>
2016-12-22 12:10:36 -0500

    add click actions

---
+++ src/actions/index.js
@@ -0,0 +1,21 @@
+export const CLICK_CELL = 'CLICK_CELL';
+export const FLAG_CELL = 'FLAG_CELL';
+
+let clickCount = 0;
+
+export const clickCell = (x, y) => {
+  return {
+    type: CLICK_CELL,
+    x,
+    y,
+    firstClick: (clickCount++ === 0),
+  };
+};
+
+export const flagCell = (x, y) => {
+  return {
+    type: FLAG_CELL,
+    x,
+    y,
+  };
+};
