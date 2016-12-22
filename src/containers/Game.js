82d0ee8a992ad2ee82780317ffb9589ba7e2f5b4
dongy7 <dongy7@gmail.com>
2016-12-22 18:35:59 -0500

    add navbar

--- src/containers/Game.js
+++ src/containers/Game.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import { grey400 } from 'material-ui/styles/colors';
 import GameBoard from './GameBoard';
 
+injectTapEventPlugin();
+
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: grey400,
+  },
+});
+
 const Game = () => (
-  <div>
-    <GameBoard />
-  </div>
+  <MuiThemeProvider muiTheme={muiTheme}>
+    <div>
+      <AppBar title="Minesweeper" />
+      <GameBoard />
+    </div>
+  </MuiThemeProvider>
 );
 
 export default Game;
