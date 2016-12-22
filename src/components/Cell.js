a9047ff77264fb33df70858048c042f39a28698a
dongy7 <dongy7@gmail.com>
2016-12-22 10:59:43 -0500

    add cell UI component

---
+++ src/components/Cell.js
@@ -0,0 +1,47 @@
+import React, { PropTypes } from 'react';
+
+const EmptyCell = () => {
+  const emptyStyle = {
+    background: '#7da3c9',
+  };
+
+  return (
+    <td
+      style={emptyStyle}
+    />
+  );
+};
+
+const CountCell = ({ count }) => {
+  return (
+    <td>
+      {count}
+    </td>
+  );
+};
+
+CountCell.propTypes = {
+  count: PropTypes.number.isRequired,
+};
+
+const BombCell = () => {
+  const bombStyle = {
+    background: '#FF6666',
+  };
+
+  return (
+    <td
+      style={bombStyle}
+    />
+  );
+};
+
+const Cell = ({ isBomb, bombCount }) => {
+  const cell = isBomb ? BombCell() : (bombCount === 0 ? EmptyCell() : CountCell(bombCount));
+  return cell;
+};
+
+Cell.propTypes = {
+  isBomb: PropTypes.bool.isRequired,
+  bombCount: PropTypes.number.isRequired,
+};
